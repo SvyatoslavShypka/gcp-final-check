2dbf193e71fda12d445aa198fb6b6499dd4608e5
Svyatoslav Shypka <svyat200909@gmail.com>
2023-08-06 11:41:35 +0200

    Ready for test2

--- src/main/java/com/goit/finalproject/FinalProjectApplication.java
+++ src/main/java/com/goit/finalproject/FinalProjectApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinalProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinalProjectApplication.class, args);
+
     }
 }
